390155cc89ec1d3bd9e93291bb58fcdc910e3f5c
natenka <nataliya.samoylenko@gmail.com>
2016-08-14 11:48:07 +0300

    Update config file

--- main-app/config.py
+++ main-app/config.py
@@ -4,9 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'top most secret!'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     BASE_PATH = os.path.abspath(os.path.dirname(__file__))+'/'
     GD_PATH = BASE_PATH + ''
@@ -27,11 +24,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                     'sqlite:///' + os.path.join(Config.BASE_PATH, 'user_info.sqlite3')
 
