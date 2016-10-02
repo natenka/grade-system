fb5011d50085f8b2cb01f7465e975d60ff74347c
natenka <nataliya.samoylenko@gmail.com>
2016-10-02 17:43:36 +0300

    Add Flask-ErrorMail

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,14 @@ from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
 from flask.ext.migrate import Migrate
 from flask.ext.mail import Mail
+from flask.ext.errormail import mail_on_500
 
 from config import config
+
+ADMINISTRATORS = (
+    "nataliya.samoylenko@gmail.com",
+)
+
 
 bootstrap = Bootstrap()
 moment = Moment()
@@ -38,6 +44,7 @@ def create_app(config_name):
     moment.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+    mail_on_500(app, ADMINISTRATORS)
 
     # import blueprints
     from .main import main as main_blueprint
